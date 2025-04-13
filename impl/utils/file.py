d8c305caaab6f17d1cfd79de110a46adacffd88c
KenEzekiel <ken35kiel@gmail.com>
2025-04-13 23:32:11 +0700

    chore: indent

--- impl/utils/file.py
+++ impl/utils/file.py
@@ -3,10 +3,20 @@ import json
 from typing import Any
 from utils.logger import logger
 
-def write_file(self, data: Any, filename: str) -> None:
-    os.makedirs('data', exist_ok=True)
-    output_path = os.path.join('data', filename)
-    with open(output_path, 'w') as file:
-        json.dump(data, file, indent=2)
-    file_logger = logger.getChild("file")
-    file_logger.info(f"Results written to {output_path}")
+def write_file(data: Any, filename: str) -> None:
+  os.makedirs('data', exist_ok=True)
+  output_path = os.path.join('data', filename)
+  with open(output_path, 'w') as file:
+      json.dump(data, file, indent=2)
+  file_logger = logger.getChild("file")
+  file_logger.info(f"Results written to {output_path}")
+
+def load_file(filename: str) -> Any:
+  input_path = os.path.join('data', filename)
+  if not os.path.exists(input_path):
+      raise FileNotFoundError(f"File {input_path} does not exist")
+  with open(input_path, 'r') as file:
+      data = json.load(file)
+  file_logger = logger.getChild("file")
+  file_logger.info(f"Loaded data from {input_path}")
+  return data
